677c05e2af202ae85633848de544ffaf5c5ab4dc
da <108029568+Mhabd@users.noreply.github.com>
2025-05-06 00:03:27 +0000

    carbara

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/download" element={<Download />} />
       </Routes>
     </Router>
