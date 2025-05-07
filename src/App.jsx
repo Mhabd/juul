c717960dff78199cef5151260d04689fcabde656
da <108029568+Mhabd@users.noreply.github.com>
2025-05-07 23:12:30 +0000

    carbara

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Homepage.jsx';
 import Download from './components/Download.jsx';
 
 function App() {
+  const [scale, setScale] = useState(1);
+
+  useEffect(() => {
+    const scaleSite = () => {
+      const baseWidth = 1920;
+      const baseHeight = 1080;
+      const scaleX = window.innerWidth / baseWidth;
+      const scaleY = window.innerHeight / baseHeight;
+      setScale(Math.min(scaleX, scaleY));
+    };
+
+    window.addEventListener('resize', scaleSite);
+    scaleSite();
+
+    return () => window.removeEventListener('resize', scaleSite);
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/download" element={<Download />} />
-    </Routes>
+    <div
+      style={{
+        transform: `scale(${scale})`,
+        transformOrigin: 'top left',
+        width: '1920px',
+        height: '1080px',
+        overflow: 'hidden',
+        position: 'relative',
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/download" element={<Download />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
